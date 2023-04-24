e8be2b19e26415caa66358f97762139479665d09
oyangjo <joyangseph@gmail.com>
2023-04-24 15:05:35 -0700

    Added temperature and fixed bug for quitting

--- multi_conversation.py
+++ multi_conversation.py
@@ -2,21 +2,31 @@ import openai
 
 '''
 Inspired by: https://www.haihai.ai/chatgpt-api/
+Tweaked by adding creativity portion to change temperature
+Also, fixed bug for input quitting string
 '''
 
 openai.api_key = 'sk-yourapikey'
 
-messages = []
-chatbotType = input("What type of chatbot would you like to develop? ")
-messages.append({"role": "system", "content": chatbotType})
+#initialization for params
+chat_history, temperature = [], 0
+chatbotType = input("What type of chatbot would you like to develop? ") 
+while temperature > 10 or temperature < 1:
+    temperature = int(input("How creative do you want it to be 1-10? (Default is 5): "))
+chat_history.append({"role": "system", "content": chatbotType})
 
+#begin conversation
 print("Your chatbot is now ready! Type \'q!\' to end conversation.")
-while input != "q!": 
+while True: 
     message = input()
-    messages.append({"role": "user", "content": message})
+    if message == 'q!': #break if quit
+        break
+
+    chat_history.append({"role": "user", "content": message}) 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages)
+        model="gpt-3.5-turbo", temperature = int(temperature)/5, messages=chat_history
+        )
+
     reply = response["choices"][0]["message"]["content"]
-    messages.append({"role": "assistant", "content": reply})
+    chat_history.append({"role": "assistant", "content": reply})
     print("\n" + reply + "\n")
