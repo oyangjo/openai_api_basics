136fee5856f87dbbd459d11acd1de75d3fa8e462
oyangjo <joyangseph@gmail.com>
2023-04-24 15:26:41 -0700

    Add comments

--- multi_conversation.py
+++ multi_conversation.py
@@ -14,6 +14,7 @@ def get_chatbot_type():
 def get_temperature():
     while True:
         try:
+            # Get user input for temperature and try converting it to an integer
             temperature = int(input("How creative do you want it to be 1-10? (Default is 5): "))
             if 1 <= temperature <= 10:
                 return temperature
@@ -21,7 +22,9 @@ def get_temperature():
             pass
 
 def main():
+    # Initialize chat_history with system message
     chat_history = [{"role": "system", "content": get_chatbot_type()}]
+    # Get temperature value and normalize it
     temperature = get_temperature() / 5
 
     print("Your chatbot is now ready! Type \'q!\' to end conversation.")
@@ -29,12 +32,15 @@ def main():
         message = input()
         if message == 'q!':
             break
-
+        
+        # Add user message to chat_history
         chat_history.append({"role": "user", "content": message})
+        # Generate chatbot response
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo", temperature=temperature, messages=chat_history
         )
 
+        # Extract and display reply
         reply = response["choices"][0]["message"]["content"]
         chat_history.append({"role": "assistant", "content": reply})
         print("\n" + reply + "\n")
